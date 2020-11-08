1756522a60d6ff5fa9947faf7f946be1c55f63dd
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-11-08 16:44:58 -0500

    updated landing page/home, added react-bootstrap

---
+++ client/src/components/StartButton/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function StartButton() {
+    return (
+        <a href="/login" className="get-started-btn">Get Started</a>
+    )
+}
+
+export default StartButton;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Button, Container } from "@material-ui/core";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "../components/Nav";
+import StartButton from "../components/StartButton";
+import "./style.css";
 
 function Home() {
     return (
-        <Container>
-            <p>Home Test</p>
-            <Button variant="contained" color="default" href="/login">Login</Button>
-        </Container>
+        <div>
+            <Nav/>
+            <div id="hero">
+                <Container fluid>
+                    <Row>
+                        <Col lg={6}>
+                            <h1>POC-IT</h1>
+                            <h2>Say hi to personalized, point of care healthcare all within your pocket</h2>
+                            <div>
+                                <StartButton />
+                            </div>
+                        </Col>
+                        <Col lg={6} className="hero-img">
+                            <img src={process.env.PUBLIC_URL + "/assets/hero.png"} className="animated" alt="provider and patient" />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </div>
+
     )
 }
 
