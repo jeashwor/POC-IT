c27f6d452b22db7bd87322894e1d045c9fa79409
Jacob Ashworth <jeashwor@gmail.com>
2020-11-13 16:34:07 -0500

    fixing safari issue

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
 const initialState = {};
@@ -9,9 +10,8 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
