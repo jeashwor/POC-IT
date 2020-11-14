970380853e4e810fa1a565f4a41488ddfd9ff42c
Jacob Ashworth <jeashwor@gmail.com>
2020-11-14 14:00:04 -0500

    completed clinician route restriction

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
 import errorReducer from "./errorReducers";
+import userReducer from "./userReducers";
 
 export default combineReducers({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    user: userReducer
 });
+
