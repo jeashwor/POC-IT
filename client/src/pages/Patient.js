a6a6224146cea1a7476093b0f91eb7b4ba6614e9
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-11-14 11:51:14 -0500

    initialized axios call for patient

--- client/src/pages/Patient.js
+++ client/src/pages/Patient.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "../components/Nav";
 import CareButton from "../components/CareButton";
 import "./style.css";
+import API from "../utils/API";
 
 function Patient() {
+    const [user, setUser] = useState("");
+    const [id, setId] = useState("");
+
+    API.getUser()
+    .then(res => {
+        setUser(res.data[0].name);
+        setId(res.data[0]._id);
+    })
+    .then(
+        API.getUserInfo(id)
+        .then(res => {
+            console.log(res);
+        })
+    )
+    
     return (
         <div>
             <Nav />
             <div className="block">
                 <Container fluid>
-                            <h1>Hi Jack!</h1>
+                            <h1>Hi {user}</h1>
                             <h2>Here's your home care regimen</h2>
                             <div>
+                                {/* replace with patient procedure assigned, data from axios call */}
                                 <CareButton image={process.env.PUBLIC_URL + "/assets/wound.png"} title="Wound Care" text="A step-by-step guide for wound dressing change using clean technique" link="/intro"/>
                             </div>
                 </Container>
---
+++ client/src/utils/API.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API = {
+    // Get patient currently logged in
+    getUser: function () {
+        return axios.get("/api/users/");
+    },
+    // Get patient info like procedures assigned to the patient
+    getUserInfo: function (id) {
+    return axios.get("/api/users/" + id);
+    }
+};
+
+export default API;
