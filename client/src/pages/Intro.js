69cc3ad113b363dd8c12feab8412aac0c156de47
Jacob Ashworth <jeashwor@gmail.com>
2020-11-18 18:34:00 -0500

    reverting back to 3f02e73c40171cfc35153e7ef1153fdcafa57a5e

--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import Container from "react-bootstrap/Container";
 import AppBar from "../components/AppBar";
 // import Nav from "../components/Nav";
@@ -8,6 +9,11 @@ import "./style.css";
 import HandGest from "../handGestures/HandGest";
 
 function Intro() {
+    // eslint-disable-next-line no-unused-vars
+    const [loading, setLoading] = useState(true);
+    const procedures = useSelector(state => state.user.user.currentProcedures);
+    let prepArr = (!procedures) ? [{title: "No Entries to List", text: "No Entries Found"}] : procedures[0].preparation;
+
     return (
         <div>
             <AppBar />
@@ -15,20 +21,12 @@ function Intro() {
                 <Container fluid>
                     <h1>Wound Care</h1>
                     <h2>Let's make sure you're ready to begin.</h2>
-                    {/* preparationArray.map((step, index) => {
-                        return <StepCard title=`{index+1}. {step.title}` text={step.description} />
-
-                        remove the StepCards below
-                    }) */}
-                    <StepCard title="1. Gather Supplies Needed" text="Saline solution to clean the wound, Sterile gloves, Sterile gauze or wound dressings, Wound tape or surgical adhesive tape, Topical ointments or antibiotics per your provider, Scissors." />
-                    <StepCard title="2. Wash your hands" text="Make sure to scrub all portions of your hands completely: the palms, the back, each finger, and in between each finger." />
-                    <StepCard title="3. Put on sterile gloves" text="Always wear sterile gloves when handling an open wound, cut or burned skin." />
-                    <StepCard title="4. Let POC-IT know you're ready" text="Facing the camera, give POC-IT a thumbs up to begin, or click the button below." />
+                    {prepArr.map((step, index) => <StepCard title={(index + 1) + ". " + step.title} text={step.step} />)}
                     <div className="text-center">
                         <StartButton  link="/procedure" label="Start"/>
                     </div>
                     <div className="handgest">
-                        <HandGest />
+                        <HandGest setLoading={setLoading} />
                     </div>
                 </Container>
             </div>
